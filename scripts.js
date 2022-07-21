6c216c5d067d648623355121396e2dc94de9ebaf
Michal Bressa (bressam) <michal.bressa@gmail.com>
2022-07-21 23:13:37 +0100

    Numeric buttons now adds text to the text area

--- scripts.js
+++ scripts.js
@@ -8,3 +8,40 @@ do math when pressed
 
 
 */
+
+const display = document.getElementById("textArea");
+const numericButtons = document.querySelectorAll(".numericButons");
+
+function buttonPressed(buttonText){
+    display.textContent = display.textContent + buttonText;
+} 
+// numeric buttons pressed
+numericButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        buttonPressed(button.textContent);
+    });
+});
+
+//special / operation buttons pressed
+const specialButtons = document.querySelectorAll(".specialButons");
+specialButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        switch (button.textContent) {
+            case "AC":
+                display.textContent = "";
+                break;
+        }
+    });
+});
+
+/*
+schemat
+const firstNumber = display.textContent  // check if is int / float
+https://dev.to/sanchithasr/7-ways-to-convert-a-string-to-number-in-javascript-4l
+bool? operationChosen = true;
+
+if (operationChosen){
+    
+}
+*/
+
