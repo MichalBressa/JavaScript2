43a9e12df123f533051f0fbacbb0e0e56e6bca73
Michal Bressa (bressam) <michal.bressa@gmail.com>
2022-07-23 18:17:20 +0100

    Adding more functions, mathematic operations are now working

--- scripts.js
+++ scripts.js
@@ -1,44 +1,196 @@
-/*
-
-textareaObject.value = textareaObject.value + button text
-
-or 
-
-do math when pressed
-
-
-*/
-
 const display = document.getElementById("textArea");
 const numericButtons = document.querySelectorAll(".numericButons");
+const dotBtn= document.getElementById("dotButton");
+let number1 = null;
+
+// numeric buttons pressed
+numericButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(button.value != "="){
+            if(button.value == "."){
+                buttonPressed(button.textContent);
+                document.querySelector(dotBtn).disabled = true;
+            }else {
+                buttonPressed(button.textContent);
+            }
+            
+        }
+        else
+        {
+            if (document.getElementById('multiplication').checked && number1 != null) {
+                let text = display.textContent;
+                let number2 = parseFloat(text);
+                display.textContent = multiply(number1, number2);
+                document.querySelector(dotBtn).disabled = false;
+            } else if (document.getElementById('division').checked && number1 != null){
+                let text = display.textContent;
+                let number2 = parseFloat(text);
+                display.textContent = divide(number1, number2);
+                document.querySelector(dotBtn).disabled = false;
+            } else if (document.getElementById('addition').checked && number1 != null){
+                let text = display.textContent;
+                let number2 = parseFloat(text);
+                display.textContent = addition(number1, number2);
+                document.querySelector(dotBtn).disabled = false;
+            } else if (document.getElementById('subtraction').checked && number1 != null){
+                let text = display.textContent;
+                let number2 = parseFloat(text);
+                display.textContent = subtraction(number1, number2);
+                document.querySelector(dotBtn).disabled = false;
+            } else if (document.getElementById('modulus').checked && number1 != null){
+                let text = display.textContent;
+                let number2 = parseFloat(text);
+                display.textContent = modulus(number1, number2);
+                document.querySelector(dotBtn).disabled = false;
+            }
+            uncheckAll();
+        }
+        
+    });
+});
 
 function buttonPressed(buttonText){
     display.textContent = display.textContent + buttonText;
 } 
-// numeric buttons pressed
-numericButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        buttonPressed(button.textContent);
+
+// operation radio buttons pressed
+const radioButtons = document.querySelectorAll('input[name="operation"]');
+radioButtons.forEach((input) => {
+    input.addEventListener('click', () => {
+        let text = display.textContent;
+        number1 = parseFloat(text);
+        display.textContent = "";
     });
 });
 
-//special / operation buttons pressed
+//special
 const specialButtons = document.querySelectorAll(".specialButons");
 specialButtons.forEach((button) => {
     button.addEventListener('click', () => {
         switch (button.textContent) {
             case "AC":
                 display.textContent = "";
+                number1 = null;
+                uncheckAll();
+                document.querySelector(dotBtn).disabled = false;
+                break;
+            case "DEL":
+                display.textContent = display.textContent.slice(0, -1);
+                break;
+            case "+/-":
+                try {
+                    let text = display.textContent;
+                    let number = parseFloat(text) * -1;
+                    display.textContent = number;
+                } catch (e) {
+                    prompt("Error, invalid input");
+                }
                 break;
         }
     });
 });
 
+
+
+// add sanity checks
+function multiply(num1, num2){
+    try {
+        num1 = parseFloat(num1);
+    } catch (e) {
+        prompt("Error, invalid input");
+    }
+
+    try {
+        num2 = parseFloat(num2);
+    } catch (e) {
+        prompt("Error, invalid input");
+    }
+
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+
+    try {
+        num1 = parseFloat(num1);
+    } catch (e) {
+        prompt("Error, invalid input");
+    }
+
+    try {
+        num2 = parseFloat(num2);
+    } catch (e) {
+        prompt("Error, invalid input");
+    }
+
+    if (num2 == 0){
+        prompt("Do not divide by 0");
+        return 0;
+    }
+
+    return num1/num2;
+}
+
+function addition(num1, num2){
+    try {
+        num1 = parseFloat(num1);
+    } catch (e) {
+        prompt("Error, invalid input");
+    }
+
+    try {
+        num2 = parseFloat(num2);
+    } catch (e) {
+        prompt("Error, invalid input");
+    }
+
+    return num1+num2;
+}
+
+function subtraction(num1, num2){
+    try {
+        num1 = parseFloat(num1);
+    } catch (e) {
+        prompt("Error, invalid input");
+    }
+
+    try {
+        num2 = parseFloat(num2);
+    } catch (e) {
+        prompt("Error, invalid input");
+    }
+
+    return num1-num2;
+}
+
+function modulus(num1, num2){
+    try {
+        num1 = parseFloat(num1);
+    } catch (e) {
+        prompt("Error, invalid input");
+    }
+
+    try {
+        num2 = parseFloat(num2);
+    } catch (e) {
+        prompt("Error, invalid input");
+    }
+
+    return num1 % num2;
+}
+
+
+function uncheckAll(){
+    const radiobtns = document.getElementsByName("operation");
+    for(var i=0;i<radiobtns.length;i++)
+      radiobtns[i].checked = false;
+}
 /*
 schemat
 const firstNumber = display.textContent  // check if is int / float
 https://dev.to/sanchithasr/7-ways-to-convert-a-string-to-number-in-javascript-4l
 bool? operationChosen = true;
+
 
 if (operationChosen){
     
